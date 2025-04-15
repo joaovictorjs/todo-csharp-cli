6ce9e25ff74586e5e67f7cfb3ab8d5dd0e6e70f8
jotave <skai7ami@gmail.com>
2025-04-15 07:19:03 -0300

    create validation exception

---
+++ ToDo.Domain/Exceptions/ValidationException.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace ToDo.Domain.Exceptions;
+
+public class ValidationException : Exception
+{
+    public ValidationException(string message)
+        : base(message) { }
+
+    public ValidationException(string message, Exception innerException)
+        : base(message, innerException) { }
+}
