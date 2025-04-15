161fa06265a3bd1ab516906c1257603e8486e704
jotave <skai7ami@gmail.com>
2025-04-15 07:12:49 -0300

    initial commit

---
+++ ToDo.Domain.Validators.UnitTests/UnitTest1.cs
@@ -0,0 +1,10 @@
+﻿namespace ToDo.Domain.Validators.UnitTests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+
+    }
+}
---
+++ ToDo.Domain.Validators/Class1.cs
@@ -0,0 +1,6 @@
+﻿namespace ToDo.Domain.Validators;
+
+public class Class1
+{
+
+}
