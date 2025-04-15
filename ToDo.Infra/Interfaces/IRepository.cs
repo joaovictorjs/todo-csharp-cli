b1cc48c74e708d200d63dbd6ef14ada2dc2ea6de
jotave <skai7ami@gmail.com>
2025-04-15 13:21:09 -0300

    implement crud operations

--- ToDo.Infra/Interfaces/IRepository.cs
+++ ToDo.Infra/Interfaces/IRepository.cs
@@ -1,6 +1,13 @@
+using System.Linq.Expressions;
 using ToDo.Domain.interfaces;
 
 namespace ToDo.Infra.Interfaces;
 
 public interface IRepository<TEntity>
-    where TEntity : IEntity { }
+    where TEntity : IEntity
+{
+    Task<int> CreateAsync(TEntity entity);
+    Task<List<TEntity>> ReadAsync(Expression<Func<TEntity, bool>> whereClause);
+    Task<int> UpdateAsync(TEntity entity);
+    Task<int> DeleteAsync(TEntity entity);
+}
--- ToDo.Infra/Repositories/Repository.cs
+++ ToDo.Infra/Repositories/Repository.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Linq.Expressions;
 using Microsoft.EntityFrameworkCore;
 using ToDo.Domain.interfaces;
 using ToDo.Infra.Contexts;
@@ -8,4 +9,32 @@ namespace ToDo.Infra.Repositories;
 
 public class Repository<TEntity>(IDbContextFactory<SqliteContext> sqliteFactory)
     : IRepository<TEntity>
-    where TEntity : IEntity { }
+    where TEntity : class, IEntity
+{
+    public async Task<int> CreateAsync(TEntity entity)
+    {
+        await using var db = await sqliteFactory.CreateDbContextAsync();
+        await db.Set<TEntity>().AddAsync(entity);
+        return await db.SaveChangesAsync();
+    }
+
+    public async Task<int> DeleteAsync(TEntity entity)
+    {
+        await using var db = await sqliteFactory.CreateDbContextAsync();
+        db.Set<TEntity>().Remove(entity);
+        return await db.SaveChangesAsync();
+    }
+
+    public async Task<List<TEntity>> ReadAsync(Expression<Func<TEntity, bool>> whereClause)
+    {
+        await using var db = await sqliteFactory.CreateDbContextAsync();
+        return await db.Set<TEntity>().AsNoTracking().Where(whereClause).ToListAsync();
+    }
+
+    public async Task<int> UpdateAsync(TEntity entity)
+    {
+        await using var db = await sqliteFactory.CreateDbContextAsync();
+        db.Set<TEntity>().Update(entity);
+        return await db.SaveChangesAsync();
+    }
+}
