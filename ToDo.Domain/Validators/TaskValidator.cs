4eaf416e2248f5ef9a9cd820f932859b8eeb176d
jotave <skai7ami@gmail.com>
2025-04-16 12:52:35 -0300

    validating task creation

--- ToDo.Domain/Validators/TaskValidator.cs
+++ ToDo.Domain/Validators/TaskValidator.cs
@@ -1,12 +1,41 @@
-﻿using ToDo.Domain.Enums;
+﻿using System.ComponentModel.DataAnnotations;
+using ToDo.Domain.Enums;
+using ToDo.Domain.Extensions;
 using ToDo.Domain.Interfaces;
 
 namespace ToDo.Domain.Validators;
 
 public class TaskValidator(IRepository<Models.Task> repository) : ITaskValidator
 {
-    public Task Validate(Operation operation, Models.Task task)
+    public async Task Validate(Operation operation, Models.Task task)
     {
-        throw new NotImplementedException();
+        switch (operation)
+        {
+            case Operation.Create:
+                {
+                    await ValidateCreation(task);
+                }
+                break;
+            default:
+                throw new ArgumentOutOfRangeException(nameof(operation), operation, null);
+        }
+    }
+
+    private async Task ValidateCreation(Models.Task task)
+    {
+        if (task.Name.IsNullOrWhiteSpace())
+        {
+            throw new ValidationException("Task name cannot be empty.");
+        }
+        
+        if (await ValidateNameExists(task))
+        {
+            throw new ValidationException("This name is already in use.");
+        }
+    }
+
+    private async Task<bool> ValidateNameExists(Models.Task task)
+    {
+        return (await repository.ReadAsync(it => it.Name == task.Name)).Count != 0;
     }
 }
---
+++ ToDo.Domain/interfaces/ITaskValidator.cs
@@ -0,0 +1,8 @@
+﻿using ToDo.Domain.Enums;
+
+namespace ToDo.Domain.Interfaces;
+
+public interface ITaskValidator
+{
+    Task Validate(Operation operation, Models.Task task);
+}
