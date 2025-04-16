aa0c5c679a42a6197ada9a91e4d504cf04062a75
joaovictorjs <skai7ami@gmail.com>
2025-04-15 22:03:46 -0300

    create app layer with argument handler

---
+++ ToDo.App/Program.cs
@@ -0,0 +1,12 @@
+﻿using System.CommandLine;
+
+namespace ToDo.App;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        var argumentHandler = new ProgramArgumentHandler();
+        argumentHandler.Handle(args);
+    }
+}
