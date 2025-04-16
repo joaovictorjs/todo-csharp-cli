56e79f28cea8dabaad4b41d1ee6dbcd7817a30b4
jotave <skai7ami@gmail.com>
2025-04-16 12:36:44 -0300

    creating task validator

---
+++ ToDo.Domain/Validators/TaskValidator.cs
@@ -0,0 +1,12 @@
+﻿using ToDo.Domain.Enums;
+using ToDo.Domain.Interfaces;
+
+namespace ToDo.Domain.Validators;
+
+public class TaskValidator(IRepository<Models.Task> repository) : ITaskValidator
+{
+    public Task Validate(Operation operation, Models.Task task)
+    {
+        throw new NotImplementedException();
+    }
+}
