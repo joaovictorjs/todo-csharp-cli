cacd4dd3bde499eda5c0a472507f08de2cb6a90c
jotave <skai7ami@gmail.com>
2025-04-17 13:25:26 -0300

    create global exception handler

---
+++ ToDo.Domain/Exceptions/ExceptionExtensions.cs
@@ -0,0 +1,11 @@
+﻿using System.Reflection.Metadata;
+
+namespace ToDo.Domain.Exceptions;
+
+public static class ExceptionExtensions
+{
+    public static void Handle(this Exception exception, Action<string> execute)
+    {
+        GlobalExceptionHandler.Handle(exception, execute);
+    }
+}
---
+++ ToDo.Domain/Exceptions/GlobalExceptionHandler.cs
@@ -0,0 +1,20 @@
+﻿using System.Runtime.ExceptionServices;
+
+namespace ToDo.Domain.Exceptions;
+
+public static class GlobalExceptionHandler
+{
+    public static void Handle(Exception exception, Action<string> execute)
+    {
+        switch (exception)
+        {
+            case ValidationException validationException:
+                execute(validationException.Message);
+                break;
+
+            default:
+                ExceptionDispatchInfo.Capture(exception).Throw();
+                break;
+        }
+    }
+}
