5d155a0770a70a0e84b86fe1e5b4e8f4cab5870d
jotave <skai7ami@gmail.com>
2025-04-17 13:08:41 -0300

    configure model creating

--- ToDo.Infra/Contexts/SqliteContext.cs
+++ ToDo.Infra/Contexts/SqliteContext.cs
@@ -1,7 +1,14 @@
 using Microsoft.EntityFrameworkCore;
+using TaskModel = ToDo.Domain.Models.Task;
 
 namespace ToDo.Infra.Contexts;
 
 public class SqliteContext(DbContextOptions<SqliteContext> options) : DbContext(options)
 {
+    public DbSet<TaskModel> Tasks { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<TaskModel>(entity => entity.HasIndex(t => t.Name).IsUnique());
+    }
 }
