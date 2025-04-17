2f59c83813005b72238435cb9567fbf8cf40aa1f
jotave <skai7ami@gmail.com>
2025-04-17 15:45:42 -0300

    implement reading

--- ToDo.Domain/interfaces/ITaskService.cs
+++ ToDo.Domain/interfaces/ITaskService.cs
@@ -1,8 +1,9 @@
-﻿using TaskMoldel = ToDo.Domain.Models.Task;
+﻿using TaskModel = ToDo.Domain.Models.Task;
 
 namespace ToDo.Domain.Interfaces;
 
 public interface ITaskService
 {
-    Task<bool> CreateAsync(TaskMoldel task);
+    Task<bool> CreateAsync(TaskModel task);
+    Task<List<TaskModel>> ReadAsync(string? name, string? description, bool? done);
 }
--- ToDo.Infra/Services/TaskService.cs
+++ ToDo.Infra/Services/TaskService.cs
@@ -1,4 +1,6 @@
-﻿using ToDo.Domain.Enums;
+﻿using System.Linq.Expressions;
+using Microsoft.EntityFrameworkCore;
+using ToDo.Domain.Enums;
 using ToDo.Domain.Interfaces;
 using TaskModel = ToDo.Domain.Models.Task;
 
@@ -11,5 +13,27 @@ public class TaskService(IRepository<TaskModel> repository, ITaskValidator valid
     {
         await validator.Validate(Operation.Create, task);
         return await repository.CreateAsync(task) > 0;
+    }
+
+    public Task<List<TaskModel>> ReadAsync(string? name, string? description, bool? done)
+    {
+        var filters = new List<Expression<Func<TaskModel, bool>>>();
+        
+        if (name is not null)
+        {
+            filters.Add(it => EF.Functions.Like(it.Name, name));
+        }
+
+        if (description is not null)
+        {
+            filters.Add(it => EF.Functions.Like(it.Description, description));
+        }
+        
+        if (done is not null)
+        {
+            filters.Add(it => it.IsDone == done);
+        }
+        
+        return repository.ReadAsync(filters.ToArray());
     }
 }
