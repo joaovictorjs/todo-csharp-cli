8a35bd9a2ba94e985b52b98e0850e6f9a1919868
jotave <skai7ami@gmail.com>
2025-04-17 13:08:18 -0300

    create design time factory

---
+++ ToDo.Infra/Contexts/SqliteContextDesignTimeFactory.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+
+namespace ToDo.Infra.Contexts;
+
+public class SqliteContextDesignTimeFactory : IDesignTimeDbContextFactory<SqliteContext>
+{
+    public SqliteContext CreateDbContext(string[] args)
+    {
+        DbContextOptionsBuilder<SqliteContext> builder = new();
+        builder.UseSqlite("Data Source=./todo.sqlite");
+        return new SqliteContext(builder.Options);
+    }
+}
