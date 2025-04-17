5e45ac1b9b4939acefdf554c9d0ab3808235ae92
jotave <skai7ami@gmail.com>
2025-04-17 08:25:33 -0300

    creating injector

---
+++ ToDo.App/Injector.cs
@@ -0,0 +1,22 @@
+﻿using Autofac;
+
+namespace ToDo.App;
+
+public class Injector
+{
+    private static Injector? _instance = null;
+    public static Injector Instance => _instance ??= new Injector();
+    private IContainer? _container = null;
+    private ContainerBuilder _containerBuilder = new();
+
+    private Injector()
+    {
+        _container = _containerBuilder.Build();
+    }
+
+    public T Resolve<T>() where T : notnull
+    {
+        ArgumentNullException.ThrowIfNull(_container);
+        return _container.Resolve<T>();
+    }
+}
