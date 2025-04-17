c16e7d1a288f49f1a37c8972cd110ca3deb998fc
jotave <skai7ami@gmail.com>
2025-04-17 08:35:23 -0300

    make methods not static and add TaskService as dependency

--- ToDo.App/Commands/NewTaskCommand.cs
+++ ToDo.App/Commands/NewTaskCommand.cs
@@ -1,16 +1,17 @@
 using System.CommandLine;
+using ToDo.Domain.Interfaces;
 
 namespace ToDo.App.Commands;
 
-public class NewTaskCommand
+public class NewTaskCommand(ITaskService taskService)
 {
-    private static readonly Option<string> _nameOption = new(["-n", "--name"], "Task name")
+    private readonly Option<string> _nameOption = new(["-n", "--name"], "Task name")
     {
         IsRequired = true,
         ArgumentHelpName = "value"
     };
 
-    private static readonly Option<string> _descOption = new(
+    private  readonly Option<string> _descOption = new(
         ["-d", "--desc"],
         "Task description"
     )
@@ -18,10 +19,10 @@ public class NewTaskCommand
         ArgumentHelpName = "value"
     };
 
-    private static string _name = string.Empty;
-    private static string _desc = string.Empty;
+    private  string _name = string.Empty;
+    private  string _desc = string.Empty;
 
-    public static Command Build()
+    public Command Build()
     {
         var command = new Command("new", "Creates a new task") { _nameOption, _descOption };
 
@@ -30,7 +31,7 @@ public class NewTaskCommand
         return command;
     }
 
-    private static void HandleNewTask(string name, string desc)
+    private  void HandleNewTask(string name, string desc)
     {
         // throw new NotImplementedException();
     }
