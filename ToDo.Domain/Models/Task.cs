3c382be4d4b51bee3ed4148aafac9765c3fa9c14
jotave <skai7ami@gmail.com>
2025-04-17 13:08:28 -0300

    name Task columns

--- ToDo.Domain/Models/Task.cs
+++ ToDo.Domain/Models/Task.cs
@@ -1,11 +1,18 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using ToDo.Domain.Interfaces;
 
 namespace ToDo.Domain.Models;
 
+[Table("tasks")]
 public class Task : IEntity
 {
-    public required int Id { get; set; }
+    [Column("id"), Key]
+    public int Id { get; set; }
+    [Column("name")]
     public required string Name { get; set; }
+    [Column("description")]
     public required string Description { get; set; }
+    [Column("is_done")]
     public required bool IsDone { get; set; }
 }
